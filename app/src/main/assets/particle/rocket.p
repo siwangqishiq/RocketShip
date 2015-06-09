7bf74fbf880cc080346c5cd948bbea0c890a5b1e
siwangqishiq <525647740@qq.com>
2015-06-09 16:46:15 +0800

    火箭运动粒子效果

---
+++ app/src/main/assets/particle/rocket.p
@@ -0,0 +1,154 @@
+没有主题
+- Delay -
+active: false
+- Duration - 
+lowMin: 500.0
+lowMax: 500.0
+- Count - 
+min: 9
+max: 25
+- Emission - 
+lowMin: 11.0
+lowMax: 11.0
+highMin: 40.0
+highMax: 40.0
+relative: false
+scalingCount: 1
+scaling0: 1.0
+timelineCount: 1
+timeline0: 0.0
+- Life - 
+lowMin: 200.0
+lowMax: 200.0
+highMin: 300.0
+highMax: 300.0
+relative: false
+scalingCount: 3
+scaling0: 1.0
+scaling1: 1.0
+scaling2: 0.3
+timelineCount: 3
+timeline0: 0.0
+timeline1: 0.66
+timeline2: 1.0
+- Life Offset - 
+active: false
+- X Offset - 
+active: false
+- Y Offset - 
+active: false
+- Spawn Shape - 
+shape: point
+- Spawn Width - 
+lowMin: 10.0
+lowMax: 10.0
+highMin: 30.0
+highMax: 30.0
+relative: false
+scalingCount: 1
+scaling0: 1.0
+timelineCount: 1
+timeline0: 0.0
+- Spawn Height - 
+lowMin: 0.0
+lowMax: 0.0
+highMin: 0.0
+highMax: 0.0
+relative: false
+scalingCount: 1
+scaling0: 1.0
+timelineCount: 1
+timeline0: 0.0
+- Scale - 
+lowMin: 7.0
+lowMax: 7.0
+highMin: 10.0
+highMax: 10.0
+relative: false
+scalingCount: 1
+scaling0: 1.0
+timelineCount: 1
+timeline0: 0.0
+- Velocity - 
+active: true
+lowMin: 50.0
+lowMax: 50.0
+highMin: 50.0
+highMax: 300.0
+relative: false
+scalingCount: 1
+scaling0: 1.0
+timelineCount: 1
+timeline0: 0.0
+- Angle - 
+active: true
+lowMin: 240.0
+lowMax: 300.0
+highMin: 240.0
+highMax: 300.0
+relative: false
+scalingCount: 3
+scaling0: 1.0
+scaling1: 0.0
+scaling2: 0.0
+timelineCount: 3
+timeline0: 0.0
+timeline1: 0.001
+timeline2: 1.0
+- Rotation - 
+active: true
+lowMin: 100.0
+lowMax: 100.0
+highMin: 100.0
+highMax: 100.0
+relative: false
+scalingCount: 2
+scaling0: 0.0056179776
+scaling1: 1.0
+timelineCount: 2
+timeline0: 0.0
+timeline1: 1.0
+- Wind - 
+active: false
+- Gravity - 
+active: true
+lowMin: 0.0
+lowMax: 0.0
+highMin: 0.0
+highMax: 0.0
+relative: false
+scalingCount: 1
+scaling0: 1.0
+timelineCount: 1
+timeline0: 0.0
+- Tint - 
+colorsCount: 3
+colors0: 0.84313726
+colors1: 0.8862745
+colors2: 0.94509804
+timelineCount: 1
+timeline0: 0.0
+- Transparency - 
+lowMin: 0.0
+lowMax: 0.0
+highMin: 1.0
+highMax: 1.0
+relative: false
+scalingCount: 4
+scaling0: 0.0
+scaling1: 1.0
+scaling2: 0.75
+scaling3: 0.0
+timelineCount: 4
+timeline0: 0.0
+timeline1: 0.2
+timeline2: 0.8
+timeline3: 1.0
+- Options - 
+attached: false
+continuous: true
+aligned: true
+additive: true
+behind: false
+- Image Path -
+particle.png
--- app/src/main/java/com/xinlan/rocketship/gameobj/Rocket.java
+++ app/src/main/java/com/xinlan/rocketship/gameobj/Rocket.java
@@ -2,10 +2,13 @@ package com.xinlan.rocketship.gameobj;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.ParticleEmitter;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
+import com.badlogic.gdx.utils.Array;
 import com.xinlan.rocketship.screen.GameScreen;
 
 /**
@@ -15,18 +18,28 @@ public class Rocket extends BaseObject {
     private GameScreen context;
     private TextureRegion rocketTexture;
     private Vector2 force;//计算所受合力
-    private float maxSpeed = 400f;// 最大速度
+    private float maxSpeed = 320f;// 最大速度
+    public static final float maxForce = 600f;
+
+    private ParticleEffect particle;
 
     public Rocket(GameScreen screen) {
         this.context = screen;
 
-        this.mass = 1;
+        this.mass = 0.5f;
         //145
         Texture originTexture = new Texture(Gdx.files.internal("rocket.png"));
         rocketTexture = new TextureRegion(originTexture, 0, 0, 256, 145);
 
         this.position.set(GameScreen.WORLD_WIDTH / 2, GameScreen.WORLD_HEIGHT / 2);
         this.bound.set(this.position.x, this.position.y, 50, 30);
+
+        //粒子效果载入
+        particle = new ParticleEffect();
+        particle.load(Gdx.files.internal("particle/rocket.p"), Gdx.files.internal("particle/"));
+        particle.getEmitters().first().setContinuous(true);
+        particle.setPosition(position.x - bound.width, position.y);
+        particle.start();
     }
 
     public void update(float delta) {
@@ -55,6 +68,37 @@ public class Rocket extends BaseObject {
             velocity.y = 0;
         }//end if
 
+        particle.setPosition(position.x, position.y);
+        rotateBy(heading.angle() + 180);
+        particle.update(delta);
+        if (particle.isComplete())
+            particle.reset();
+
+        if (this.velocity.len() < 50) {
+            particle.allowCompletion();
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        if (!particle.isComplete()) {
+            particle.draw(batch);
+        }
+        batch.draw(rocketTexture, this.position.x - this.bound.width / 2,
+                this.position.y - this.bound.height / 2, bound.width / 2, bound.height / 2, bound.width, bound.height, 1, 1, heading.angle());
+//        batch.draw(rocketTexture, this.position.x - this.bound.width / 2,
+//                this.position.y - this.bound.height / 2, bound.width / 2, bound.height / 2, bound.width, bound.height, 1, 1, heading.angle());
+    }
+
+    public void rotateBy(float amountInDegrees) {
+        Array<ParticleEmitter> emitters = particle.getEmitters();
+        for (int i = 0; i < emitters.size; i++) {
+            ParticleEmitter.ScaledNumericValue val = emitters.get(i).getAngle();
+            float amplitude = (val.getHighMax() - val.getHighMin()) / 2f;
+            float h1 = amountInDegrees + amplitude;
+            float h2 = amountInDegrees - amplitude;
+            val.setHigh(h1, h2);
+            val.setLow(amountInDegrees);
+        }//end for i
     }
 
     /**
@@ -65,13 +109,7 @@ public class Rocket extends BaseObject {
     private Vector2 calculateForce() {
         Vector2 retForce = new Vector2(0, 0);
         Touchpad touchPad = context.mController.touchpad;
-        float maxForce = 300f;
         retForce.add(touchPad.getKnobPercentX() * maxForce, touchPad.getKnobPercentY() * maxForce);
         return retForce;
-    }
-
-    public void render(SpriteBatch batch) {
-        batch.draw(rocketTexture, this.position.x - this.bound.width / 2,
-                this.position.y - this.bound.height / 2, bound.width / 2, bound.height / 2, bound.width, bound.height, 1, 1, heading.angle());
     }
 }//end class
